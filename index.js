72b731f0fa4cb36285327f9f8e034416f0f23398
raulrequenac <raulrequec@gmail.com>
2020-05-13 15:19:57 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const form = document.getElementById("calculator")
 
-const addToDisplay = (char) => form.display.value += char
+//Show on display the current number and update the equation
+const addToDisplay = (value) => {
+  form.equation.value += value
+  form.display.value += value
+}
+
+//Rest display
 const clearDisplay = () => form.display.value = ''
-const displayResult = () => form.display.value = eval(form.display.value)
+const clearAll = () => {
+  clearDisplay()
+  form.equation.value = ''
+}
+
+//Add operator to the equation and clear display
+const addOperator = (operator) => {
+  form.equation.value += operator
+  clearDisplay()
+}
+
+
+//Solve equation and show the result on the display
+const displayResult = () => {
+  try {
+    form.display.value = eval(form.equation.value)
+  } catch (e) {
+    form.display.value = 0
+  }
+}
